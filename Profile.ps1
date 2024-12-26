aa2a3feb6fabe02eddc0f6d31d7f4bce0f4477e3
Valdas Čereška <162885273+valdAsync@users.noreply.github.com>
2024-12-26 13:15:27 +0200

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -17,3 +17,62 @@ function back {
 New-Alias -Name '..' -Value back
 
 New-Alias -Name 'll' -Value My-Func
+
+# Git Aliases
+
+function gst {
+    git status
+}
+
+function gaa {
+    git add -A
+}
+
+function gc {
+    git commit @args
+}
+
+function gcm {
+    git checkout main
+}
+
+function gd {
+    git diff
+}
+
+function gdc {
+    git diff --cached
+}
+
+function co {
+    git checkout @args
+}
+
+function up {
+    git push
+}
+
+function upf {
+    git push --force
+}
+
+function pu {
+    git pull
+}
+
+function pur {
+    git pull --rebase
+}
+
+function fe {
+    git fetch
+}
+
+function re {
+    git rebase
+}
+
+function glr {
+    git l -30
+}
+
